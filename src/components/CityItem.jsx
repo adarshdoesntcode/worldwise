f708349b87dc3d02c4c09b72fcaadaa76c5fdf1b
Adarsh Das <adarsh.191605@ncit.edu.np>
2023-12-21 19:10:08 +0545

    18-11-including the map

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import { useAppProvider } from "../contexts/useAppContext";
 
